e4cbc5999912e6cb4b46089df3325099e5368d45
Gianbattista Vivolo <gianbattista.vivolo@gmail.com>
2024-11-17 21:25:00 +0100

    Fixed build.gradle sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.gianby.mobileapps"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.gianby.mobileapps"
@@ -45,4 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(kotlin("script-runtime"))
 }
