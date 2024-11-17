32c136f3a37281383726ddcfa5ff352d763fbf9d
Gianbattista Vivolo <gianbattista.vivolo@gmail.com>
2024-11-17 22:06:17 +0100

    Added login functionality

--- app/src/main/java/com/gianby/mobileapps/LoginActivity.kt
+++ app/src/main/java/com/gianby/mobileapps/LoginActivity.kt
@@ -3,14 +3,28 @@ package com.gianby.mobileapps
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    val credentialsManager = CredentialsManager()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            handleLogin()
+        }
+
+
+
 
         val registerNowLabel = findViewById<TextView>(R.id.registerNowLabel)
         registerNowLabel.setOnClickListener {
@@ -19,6 +33,42 @@ class LoginActivity : AppCompatActivity() {
             goToRegisterIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(goToRegisterIntent)
             finish()
+        }
+    }
+
+    private fun handleLogin() {
+        val emailInputLayout = findViewById<TextInputLayout>(R.id.emailInputLayout)
+        val passwordInputLayout = findViewById<TextInputLayout>(R.id.passwordInputLayout)
+        val emailEditText = findViewById<TextInputEditText>(R.id.emailInput)
+        val passwordEditText = findViewById<TextInputEditText>(R.id.passwordInput)
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+
+        // Validate email
+        if (!credentialsManager.isEmailValid(email)) {
+            emailInputLayout.error = "Invalid email format"
+            return
+        } else {
+            emailInputLayout.error = null
+        }
+
+        // Validate password
+        if (!credentialsManager.isPasswordValid(password)) {
+            passwordInputLayout.error = "Password cannot be empty"
+            return
+        } else {
+            passwordInputLayout.error = null
+        }
+
+        // Check hardcoded credentials
+        if (email == "test@te.st" && password == "1234") {
+            // go to MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            emailInputLayout.error = "Invalid credentials"
+            passwordInputLayout.error = "Invalid credentials"
         }
     }
 }
