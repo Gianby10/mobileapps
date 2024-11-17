acb4829f69eee490709fcc5c99d1068aeec7ead7
Gianbattista Vivolo <gianbattista.vivolo@gmail.com>
2024-11-17 21:22:39 +0100

    Removed RegisterActivity.java and added switch to LoginActivity functionality

--- app/src/main/java/com/gianby/mobileapps/RegisterActivity.kt
+++ app/src/main/java/com/gianby/mobileapps/RegisterActivity.kt
@@ -1,4 +1,24 @@
-package com.gianby.mobileapps;
+package com.gianby.mobileapps
 
-public class RegisterActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_register)
+
+        val registerNowLabel = findViewById<View>(R.id.loginLabel)
+        registerNowLabel.setOnClickListener{
+            val goToLoginIntent = Intent(this,LoginActivity::class.java)
+            goToLoginIntent.flags  = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(goToLoginIntent)
+            finish()
+        }
+
+
+    }
 }
