12e799f41f7cccf777201b3bcf46d0d5684d7f5f
Gianbattista Vivolo <gianbattista.vivolo@gmail.com>
2024-11-17 21:23:13 +0100

    Added switch to RegisterActivity functionality

--- app/src/main/java/com/gianby/mobileapps/LoginActivity.kt
+++ app/src/main/java/com/gianby/mobileapps/LoginActivity.kt
@@ -1,13 +1,24 @@
 package com.gianby.mobileapps
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
+        val registerNowLabel = findViewById<TextView>(R.id.registerNowLabel)
+        registerNowLabel.setOnClickListener {
+
+            val goToRegisterIntent = Intent(this, RegisterActivity::class.java)
+            goToRegisterIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(goToRegisterIntent)
+            finish()
+        }
     }
 }
