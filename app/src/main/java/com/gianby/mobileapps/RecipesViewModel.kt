3ce13b01f786fc8e8792bf1ac20490c80318881d
Gianbattista Vivolo <gianbattista.vivolo@gmail.com>
2025-01-19 21:55:43 +0100

    Added code to simulate a 2s delay between searches.

--- app/src/main/java/com/gianby/mobileapps/RecipesViewModel.kt
+++ app/src/main/java/com/gianby/mobileapps/RecipesViewModel.kt
@@ -3,31 +3,59 @@ package com.gianby.mobileapps
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.myapplication.RecipesList
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
+data class RecipeUIState(
+    val isLoading: Boolean = false,
+    val recipes: List<Recipe> = emptyList()
+)
+
 class RecipesViewModel : ViewModel() {
-    private val _recipes = MutableStateFlow<List<Recipe>>(emptyList())
-    val recipes: StateFlow<List<Recipe>> = _recipes.asStateFlow()
+    private val _uiState = MutableStateFlow(RecipeUIState())
+   // private val _recipes = MutableStateFlow<List<Recipe>>(emptyList())
+    //val recipes: StateFlow<List<Recipe>> = _recipes.asStateFlow()
+    val uiState: StateFlow<RecipeUIState> = _uiState.asStateFlow()
+    private var searchJob: kotlinx.coroutines.Job? = null
 
     private val allRecipes = RecipesList.recipes
 
     init {
-        _recipes.value = allRecipes
+        viewModelScope.launch {
+            delay(2000)
+            _uiState.update { it.copy(
+                isLoading = false,
+                recipes = RecipesList.recipes
+            ) }
+        }
     }
 
-    fun searchRecipes(query: String) {
-        if (query.length < 3) {
-            _recipes.update { allRecipes }
-        } else {
-            val filteredRecipes = allRecipes.filter {
-                it.title.contains(query, ignoreCase = true) ||
-                        it.description.contains(query, ignoreCase = true)
+    fun queryRecipes(query: String) {
+        searchJob?.cancel()
+        searchJob = viewModelScope.launch {
+            _uiState.update { it.copy(isLoading = true, recipes = emptyList()) }
+
+            delay(300)
+
+            val filteredRecipes = if (query.length < 3) {
+                RecipesList.recipes
+            } else {
+                RecipesList.recipes.filter { recipe ->
+                    recipe.title.contains(query, ignoreCase = true)
+                }
             }
-            _recipes.update { filteredRecipes }
+
+            delay(2000)
+
+            _uiState.update { it.copy(isLoading = false, recipes = filteredRecipes) }
         }
+    }
+
+    fun logout() {
+        MobileApplication.getInstance().credentialsManager.logout()
     }
 }
