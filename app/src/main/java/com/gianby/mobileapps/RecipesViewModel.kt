49b4f3e7698f37cc746bd955688db4170da0f646
Gianbattista Vivolo <gianbattista.vivolo@gmail.com>
2025-01-19 21:19:17 +0100

    Removed hardcoded recipes.

--- app/src/main/java/com/gianby/mobileapps/RecipesViewModel.kt
+++ app/src/main/java/com/gianby/mobileapps/RecipesViewModel.kt
@@ -2,6 +2,7 @@ package com.gianby.mobileapps
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.myapplication.RecipesList
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,12 +13,7 @@ class RecipesViewModel : ViewModel() {
     private val _recipes = MutableStateFlow<List<Recipe>>(emptyList())
     val recipes: StateFlow<List<Recipe>> = _recipes.asStateFlow()
 
-    private val allRecipes = listOf(
-        Recipe(1, "Recipe 1", "Description 1."),
-        Recipe(2, "Recipe 2", "Description 2."),
-        Recipe(3, "Recipe 3", "Description 3."),
-        Recipe(4, "Recipe 4", "Description 4.")
-    )
+    private val allRecipes = RecipesList.recipes
 
     init {
         _recipes.value = allRecipes
