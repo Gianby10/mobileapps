cda40cbb916bddfd0e1afa1f06692cf8cda94416
Gianbattista Vivolo <gianbattista.vivolo@gmail.com>
2025-01-19 21:17:31 +0100

    Moved recipes to a dedicated file.

---
+++ app/src/main/java/com/gianby/mobileapps/RecipesList.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import com.gianby.mobileapps.Recipe
+
+object RecipesList {
+    val recipes = listOf(
+        Recipe(1, "Spaghetti Carbonara", "A classic Italian pasta dish made with eggs, cheese, pancetta, and pepper."),
+        Recipe(2, "Chicken Tikka Masala", "A flavorful Indian-inspired dish with grilled chicken in a creamy tomato sauce."),
+        Recipe(3, "Classic Caesar Salad", "A refreshing salad with romaine lettuce, croutons, and a tangy Caesar dressing."),
+        Recipe(4, "Chocolate Brownies", "Rich and fudgy chocolate brownies, perfect for a sweet treat.")
+    )
+
+}
